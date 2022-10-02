aacb9c61a36a711c1f401d7787d85c7809a7ea54
Aniruddha-Sainkar <asainkar@gmail.com>
2022-10-02 11:38:50 -0400

    trying git

---
+++ hrnew/src/hrnew/Hrnew.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hrnew;
+
+/**
+ *
+ * @author aniruddhasainkar
+ */
+public class Hrnew {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Hello Github");
+        // TODO code application logic here
+    }
+    
+}
