8199b870a9c751c49b654b93218870742ef9f673
Aniruddha-Sainkar <asainkar@gmail.com>
2022-10-02 23:05:51 -0400

    Final

---
+++ ManagementProject/src/Aniruudha/MyFrame.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aniruudha;
+
+import Aniruddha.MainFrame;
+
+/**
+ *
+ * @author Aniruddha
+ */
+public class MyFrame extends MainFrame {
+
+    public MyFrame() {
+    }
+    
+}
